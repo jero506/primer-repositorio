da3d7dfd4f73d885ad46bcda5bc4278dde2872bd
jero506 <2020jeronimo2020@gmail.com>
2026-06-17 18:16:02 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+datos = [
+    "mi aficion afovorita es jugar video juegos",
+    "tengo una mascota llamada mochi",
+    "tengo un juego tipo arcade el cual me alegra haver logrados",
+    "mi comida favorita es la pasta con pollo"
+        ]
+dato_aleatorio = random.choice(datos)
+print (dato_aleatorio)
